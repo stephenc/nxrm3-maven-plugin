19a73bbfaa5c522b3f7090c99880d0985ed352a4
Stephen Connolly <stephen.alan.connolly@gmail.com>
2021-11-16 16:19:02 +0000

    Revert "Merge remote-tracking branch 'origin/nexus-29515' into cloudbees-1"
    This reverts commit 454ed545855ef4ed6889c8042096b29ae33deef3, reversing
    changes made to 0d8a8d9b9d9995610f6fe8bb27a8c4c241e8d6fd.

--- maven-plugin/src/main/java/org/sonatype/nexus/maven/staging/StagingDeployMojo.java
+++ maven-plugin/src/main/java/org/sonatype/nexus/maven/staging/StagingDeployMojo.java
@@ -51,9 +51,6 @@ public class StagingDeployMojo
   @Parameter(property = "repository", required = true)
   private String repository;
 
-  @Parameter(property = "snapshotRepository")
-  private String snapshotRepository;
-
   @Parameter(property = "tag")
   private String tag;
 
@@ -98,20 +95,13 @@ public class StagingDeployMojo
 
     failIfOffline();
 
-    String repository;
-    if (artifact.isSnapshot() && snapshotRepository != null && !snapshotRepository.trim().isEmpty()) {
-      repository = snapshotRepository;
-    } else {
-      repository = this.repository;
-    }
-
     List<Artifact> deployables = prepareDeployables();
 
     try {
       tag = getProvidedOrGeneratedTag();
       maybeCreateTag(client, tag);
       getLog().info(String.format("Deploying to repository '%s' with tag '%s'", repository, tag));
-      doUpload(client, repository, deployables, tag);
+      doUpload(client, deployables, tag);
     }
     catch (MojoExecutionException e) {
       throw e;
@@ -157,7 +147,6 @@ public class StagingDeployMojo
   }
 
   private void doUpload(final RepositoryManagerV3Client client,
-                        final String repository,
                         final List<Artifact> deployables,
                         final String tag) throws Exception
   {
@@ -270,10 +259,5 @@ public class StagingDeployMojo
   @VisibleForTesting
   void setTagGenerator(final TagGenerator tagGenerator) {
     this.tagGenerator = tagGenerator;
-  }
-
-  @VisibleForTesting
-  void setSnapshotRepository(String snapshotRepository) {
-    this.snapshotRepository = snapshotRepository;
   }
 }
